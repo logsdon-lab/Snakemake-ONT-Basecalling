ce6b05e3aa104a0ee0c17cec51afa509502f8bec
koisland <koshima789@gmail.com>
2024-05-20 12:55:47 -0400

    Rename test directories. Add rebasecalling logic.

--- basecall.sh
+++ basecall.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 # Check if command fails due to incomplete files
-grep "IncompleteFilesException" <(snakemake --summary 2>&1 || true) > /dev/null && true
+grep "IncompleteFilesException" <(snakemake -np $@ 2>&1 || true) > /dev/null && true
 ret=$?
 
 # Incomplete basecalling. Redo.
 if [[ $ret -eq 0 ]]; then
     echo "Incomplete files detected. Redoing basecalling."
-    snakemake -c1 -np --config rebasecall=True
+    snakemake -c4 -p --config rebasecall=True --rerun-incomplete $@
 else
     echo "Basecalling reads."
-    snakemake -c1 -np
+    snakemake -c4 -p $@
 fi
